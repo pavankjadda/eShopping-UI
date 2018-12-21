5418f9c4bfadeb8e815bb812752b0e1368765d71
Pavan Jadda <jpavanaryan@gmail.com>
2018-12-21 04:09:03 -0500

    Environment added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Home';
+  constructor()
+  {
+    console.log(environment.production); // Logs false for default environment
+  }
 }
