f8b0b41e7d654293130b5e01e55f7299b8d80a6d
Pavan Jadda <jpavanaryan@gmail.com>
2018-12-21 02:25:29 -0500

    Main Component Added

--- src/app/layouts/ui.module.ts
+++ src/app/layouts/ui.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
-  exports: [LayoutComponent]
+  declarations: [HeaderComponent, FooterComponent, MainComponent],
+  exports:[MainComponent]
 })
 export class UiModule { }
