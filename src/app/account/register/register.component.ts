7c87fdbb8b64e515788af7b25e47c640b942c70c
Pavan Jadda <jpavanaryan@gmail.com>
2019-01-11 22:23:57 -0500

    Both formats of validators works

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../../core/auth/auth.service';
+import {customValidator} from './username-validator.directive';
 
 @Component( {
   selector: 'app-register',
@@ -12,6 +13,7 @@ export class RegisterComponent implements OnInit
 {
   registerForm: FormGroup;
   returnUrl: string;
+  submitted: boolean;
 
   constructor(private formBuilder:FormBuilder,private route:ActivatedRoute,private router:Router, private authService:AuthService)
   {
@@ -20,7 +22,7 @@ export class RegisterComponent implements OnInit
   }
 
   // convenience getter for easy access to form fields
-  get f()
+  get formControls()
   {
     return this.registerForm.controls;
   }
@@ -29,7 +31,7 @@ export class RegisterComponent implements OnInit
   {
     this.registerForm=this.formBuilder.group(
       {
-        username: ['',Validators.required],
+        username: ['',[Validators.required,Validators.maxLength(15),customValidator()]],
         email: ['',Validators.required],
         password: ['',Validators.required],
         confirmPassword: ['',Validators.required],
@@ -39,10 +41,11 @@ export class RegisterComponent implements OnInit
 
   register()
   {
-    let username=this.f.username.value;
-    let email=this.f.email.value;
-    let password=this.f.password.value;
-    let confirmPassword=this.f.confirmPassword.value;
+    this.submitted=true;
+    let username=this.formControls.username.value;
+    let email=this.formControls.email.value;
+    let password=this.formControls.password.value;
+    let confirmPassword=this.formControls.confirmPassword.value;
 
   }
 
---
+++ src/app/account/register/username-validator.directive.ts
@@ -0,0 +1,22 @@
+import {AbstractControl, ValidatorFn} from '@angular/forms';
+
+export function customValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if (control.value !== undefined && (isNaN(control.value) || control.value.length <6))
+    {
+      return {insufficientLength: true};
+    }
+    return null;
+  };
+}
+
+
+/*
+export function customValidator(control: AbstractControl)
+{
+  if (control.value !== undefined && (isNaN(control.value) || control.value.length <6))
+  {
+    return { insufficientLength: true };
+  }
+  return null;
+}*/
