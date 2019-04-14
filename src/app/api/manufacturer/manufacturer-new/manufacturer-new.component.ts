d1b2366ff51603c583e0b0cd20f2ac6d92e7b7f5
Pavan Jadda <jpavanaryan@gmail.com>
2019-04-14 19:39:18 -0400

    manufacturer components added

--- src/app/api/manufacturer/manufacturer-new/manufacturer-new.component.ts
+++ src/app/api/manufacturer/manufacturer-new/manufacturer-new.component.ts
@@ -1,15 +1,44 @@
 import {Component, OnInit} from '@angular/core';
+import {ManufacturerService} from '../service/manufacturer.service';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MANUFACTURER_API_URL, SERVER_URL} from '../../../app.constants';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-manufacturer-new',
   templateUrl: './manufacturer-new.component.html',
   styleUrls: ['./manufacturer-new.component.css']
 })
-export class ManufacturerNewComponent implements OnInit {
+export class ManufacturerNewComponent implements OnInit
+{
+  manufacturerForm=new FormGroup(
+    {
+      id: new FormControl( {value: '', disabled: true} ),
+      name: new FormControl( '' ),
+      description: new FormControl( '' ),
+      address: new FormControl( '' ),
+      contactEmail: new FormControl( '' ),
+      fax: new FormControl( '' ),
+      products: new FormControl( '' ),
+    } );
 
-  constructor() { }
+  constructor(private manufacturerService:ManufacturerService,
+              private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
   }
 
+  createManufacturer()
+  {
+    const url=SERVER_URL+MANUFACTURER_API_URL+'create';
+
+  }
+
+
+
+  goBack()
+  {
+    this.router.navigate(['/manufacturer']);
+  }
 }
