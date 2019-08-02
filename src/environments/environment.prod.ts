a2dca328ec0d0117097d66fd937c87ce02f21519
Pavan Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-02 13:03:45 -0400

    Move SERVER_URL to environment variables and load it dynamically based on Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'http://ec2-54-152-16-196.compute-1.amazonaws.com/'
+  SERVER_URL: 'http://ec2-54-152-16-196.compute-1.amazonaws.com:8080/'
 };
