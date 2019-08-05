4f23348acfcc3424f096da1812847ffb5cbd5b03
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2019-08-05 16:17:45 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //SERVER_URL: 'http://ec2-54-152-16-196.compute-1.amazonaws.com:8080/'
-  SERVER_URL: 'https://nextgenshopping.tk/'
+  SERVER_URL: 'https://nextgen-code.com/'
 };
