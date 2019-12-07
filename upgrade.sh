6700a14937788539c600e60809e2e44bc729d362
Pavan Jadda <jpavanaryan@gmail.com>
2019-12-06 22:26:44 -0500

    Upgrade dependencies

--- upgrade.sh
+++ upgrade.sh
@@ -3,4 +3,7 @@ git commit -m "Before Upgrade"
 git push origin master
 ng update --all --force
 npm install  typescript@3.6.4 --save
+git add .
+git commit -m "After Upgrade"
+git push origin master
 #npm install
