15e537998fedc117abdbc08b0cee8158fed41944
Pavan Jadda <jpavanaryan@gmail.com>
2019-12-19 23:31:03 -0500

    Before Upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,10 @@
 git add .
 git commit -m "Before Upgrade"
-git push origin master
+git push origin "${git branch | grep \* | cut -d ' ' -f2}"
 ng update --all --force
 npm install  typescript@3.6.4 --save
 git add .
 git commit -m "After Upgrade"
-git push origin master
+git push origin "${git branch | grep \* | cut -d ' ' -f2}"
 #npm install
+
