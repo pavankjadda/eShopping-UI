5268ad4453c0959ad7baab8e3bc93e465536f6ad
Pavan Jadda <jpavanaryan@gmail.com>
2019-12-19 23:34:05 -0500

    Before Upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+### Upgrade Angular version
+CURRENT_BRANCH="${git branch | grep \* | cut -d ' ' -f2}"
+
 git add .
 git commit -m "Before Upgrade"
-git push origin "${git branch | grep \* | cut -d ' ' -f2}"
+git push origin "$CURRENT_BRANCH"
 ng update --all --force
 npm install  typescript@3.6.4 --save
 git add .
 git commit -m "After Upgrade"
-git push origin "${git branch | grep \* | cut -d ' ' -f2}"
+git push origin "$CURRENT_BRANCH"
 #npm install
 
