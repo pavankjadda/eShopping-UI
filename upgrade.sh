b7832697db4f4e117d93e265db18c9fa9231e65b
Pavan Jadda <jpavanaryan@gmail.com>
2020-02-01 17:08:20 -0500

    Upgrade to 9.0.0-rc.9

--- upgrade.sh
+++ upgrade.sh
@@ -7,6 +7,7 @@ echo "Current branch is $CURRENT_BRANCH"
 git add .
 git commit -m "Before Upgrade"
 git push origin "$CURRENT_BRANCH"
+ng update @angular/cli @angular/core --update=true
 ng update --all --force
 git add .
 git commit -m "After Upgrade"
