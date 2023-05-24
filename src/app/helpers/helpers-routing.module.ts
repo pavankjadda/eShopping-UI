c6ce6971a1c73067798af68d6333090f6f0a31aa
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-05-23 22:57:09 -0400

    fix(build): fixes build issues

--- src/app/helpers/helpers-routing.module.ts
+++ src/app/helpers/helpers-routing.module.ts
@@ -5,7 +5,7 @@ import {Http403ErrorComponent} from './http403-error/http403-error.component';
 const routes: Routes = [{path: '403', component: Http403ErrorComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {relativeLinkResolution: 'legacy'})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class HelpersRoutingModule {
