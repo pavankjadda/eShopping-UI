f965510017f8304eb680ce168c24e50238f145b0
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-06-19 22:23:54 -0400

    refactor(core): removes karma and jasmine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {environment} from '../environments/environment.prod';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Home';
